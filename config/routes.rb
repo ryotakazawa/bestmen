2b8b18983745f3d37901606f60d539bf8dfbb529
EC2 Default User <ec2-user@ip-172-31-36-180.ap-northeast-1.compute.internal>
2019-06-17 14:17:52 +0000

    made-users-controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-
-  get '/about', to:'static_pages#about'
+  get '/about',  to:'static_pages#about'
+  
+  #users
+  get '/signup', to:'users#new'
+  
   
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
---
+++ spec/features/users_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+describe 'New' do
+  specify '画面の表示' do
+    visit signup_path
+  end
+end
