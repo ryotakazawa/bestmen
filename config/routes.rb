f667700f43b05d87508046231f19c3c888fd55ba
Ryo Takazawa <ryotakazawaengineer@gmail.com>
2019-07-18 02:48:41 +0000

    implemented_bootstrap_social

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@
 
 Rails.application.routes.draw do
   
-
-  # devise_for :users
   root to: 'home#top'
   get  '/about', to: 'home#about'
 
@@ -31,7 +29,4 @@ Rails.application.routes.draw do
   
   resources :maps, only: [:index]
   
-  
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
