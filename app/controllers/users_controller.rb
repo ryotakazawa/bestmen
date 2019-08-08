dba3c67fbbda743b359e5063a95f50a15da3c726
Ryo Takazawa <ryotakazawaengineer@gmail.com>
2019-08-08 10:44:24 +0000

    showed posted pics and liked pics on user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,9 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find_by(id: params[:id])
-    @pics = Pic.where(user_id: params[:id]).includes(:user)
+    @posted_pics = Pic.where(user_id: params[:id]).page(params[:page]).per(6)
+    @liked_pics = @user.votes.up.for_type(Pic).votables
+    @liked_pics= Kaminari.paginate_array(@liked_pics).page(params[:page]).per(6)
   end
   
 end
