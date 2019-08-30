05f0683ed28840da67d1ac83510cebbea59b7156
Ryo Takazawa <ryotakazawaengineer@gmail.com>
2019-08-30 07:35:04 +0000

    now app can send custom mailer view

---
+++ app/mailers/users_mailer.rb
@@ -0,0 +1,25 @@
+class UsersMailer < Devise::Mailer
+  helper :application
+  include Devise::Controllers::UrlHelpers
+  default template_path: 'users_mailer'
+
+  # def confirmation_instructions(record, token, opts={})
+  #   super
+  # end
+
+  # def email_changed(record, token, opts={})
+  #   super
+  # end
+
+  # def password_change(record, token, opts={})
+  #   super
+  # end
+
+  # def reset_password_instructions(record, token, opts={})
+  #   super
+  # end
+
+  # def unlock_instructions(record, token, opts={})
+  #   super
+  # end
+end
