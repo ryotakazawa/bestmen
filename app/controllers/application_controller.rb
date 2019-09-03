e335ebce3d4de280d9bc2bf37061209ebda10672
Ryo Takazawa <ryotakazawaengineer@gmail.com>
2019-09-03 10:59:28 +0000

    now users/show shows profile picture

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :image])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :image])
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190722195714) do
+ActiveRecord::Schema.define(version: 20190903023819) do
 
   create_table "comments", force: :cascade do |t|
     t.string "content"
@@ -77,6 +77,10 @@ ActiveRecord::Schema.define(version: 20190722195714) do
     t.string "uid"
     t.string "provider"
     t.boolean "admin_flg"
+    t.string "image_file_name"
+    t.string "image_content_type"
+    t.bigint "image_file_size"
+    t.datetime "image_updated_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
