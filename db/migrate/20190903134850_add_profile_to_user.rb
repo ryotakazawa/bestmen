3f6d974902ae59b2e40424a502c1b2eeda3bf471
Ryo Takazawa <ryotakazawaengineer@gmail.com>
2019-09-03 14:12:53 +0000

    profile is showed on user page

---
+++ db/migrate/20190903134850_add_profile_to_user.rb
@@ -0,0 +1,5 @@
+class AddProfileToUser < ActiveRecord::Migration[5.1]
+  def change
+    add_column :users, :profile, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190903023819) do
+ActiveRecord::Schema.define(version: 20190903134850) do
 
   create_table "comments", force: :cascade do |t|
     t.string "content"
@@ -81,6 +81,7 @@ ActiveRecord::Schema.define(version: 20190903023819) do
     t.string "image_content_type"
     t.bigint "image_file_size"
     t.datetime "image_updated_at"
+    t.string "profile"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
