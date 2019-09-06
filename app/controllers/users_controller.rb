5c074e527a7c6565656e62307e0739a1df207009
Ryo Takazawa <ryotakazawaengineer@gmail.com>
2019-09-06 09:37:58 +0000

    edited views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find_by(id: params[:id])
-    @posted_pics = Pic.where(user_id: params[:id]).page(params[:page]).per(6)
+    @posted_pics = Pic.where(user_id: params[:id]).includes(:taggings).page(params[:page]).per(6)
     @liked_pics = @user.votes.up.for_type(Pic).votables
     @liked_pics= Kaminari.paginate_array(@liked_pics).page(params[:page]).per(6)
   end
