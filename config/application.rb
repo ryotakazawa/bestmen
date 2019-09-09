fb93c3888d2c337bd1e89a367b5852cc18e98076
Ryo Takazawa <ryotakazawaengineer@gmail.com>
2019-09-09 07:04:09 +0000

    edited views

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,6 @@ module Bestone
     # -- all .rb files in that directory are automatically loaded.
     config.i18n.default_locale = :ja
     config.action_view.embed_authenticity_token_in_remote_forms = true
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
   end
 end
