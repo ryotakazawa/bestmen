ce1d56bfa87a230bb2c947d66c9ee8d8e0ff39f4
Ryo Takazawa <ryotakazawaengineer@gmail.com>
2019-09-19 13:41:55 +0000

    added rspec for pic create

--- spec/support/shared_example/shared_examples_for_pic.rb
+++ spec/support/shared_example/shared_examples_for_pic.rb
@@ -4,12 +4,14 @@
 # pics#new
 # view test
 shared_examples_for 'new_pic-form have right css' do
-  it { expect(page).to have_css('label', text: '画像') }
   it { expect(page).to have_css('label', text: '店名') }
-  it { expect(page).to have_css('label', text: '説明') }
+  it { expect(page).to have_css('label', text: '画像') }
+  it { expect(page).to have_css('label', text: '住所') }
+  it { expect(page).to have_css('label', text: 'ジャンル') }
+  it { expect(page).to have_css('label', text: '認定理由') }
+  it { expect(page).to have_css('input#pic_title') }
   it { expect(page).to have_css('input#pic_image') }
-  it { expect(page).to have_css('input#pic_name') }
-  it { expect(page).to have_css('input#pic_description') }
+  it { expect(page).to have_css('input#pic_address') }
   it { expect(page).to have_button('登録する') }
 end
 
@@ -19,8 +21,7 @@ end
 shared_examples_for 'success create pic' do
   scenario 'pic increment 1' do
     expect do
-      visit new_pic_path
-      fill_in_pic_new_form(:pic) # => SupportModule を使用
+      fill_in_pic_new_form(:pic)
       click_button '登録する'
     end.to change(Pic, :count).by(1)
     # メッセージ
@@ -31,16 +32,24 @@ shared_examples_for 'success create pic' do
   end
 end
 # failed
-shared_examples_for 'fail create pic' do
+# name blank
+shared_examples_for 'fail create pic name blank' do
   scenario 'user increment 0' do
     expect do
-      visit new_pic_path
-      fill_in_pic_new_form(:pic, invalid: true) # => SupportModule を使用
+      fill_in_pic_new_form(:pic, nameblank: true)
       click_button '登録する'
     end.to change(Pic, :count).by(0)
-    # メッセージ
-    # expect(page).to have_css('div.alert.alert-danger', text: 'errors')
-    # サインアップページがrender
+    expect(page).to have_title('認定する | BESTMEN -1番だけをシェアしよう-')
+    expect(page).to have_css('h1', text: '認定する')
+  end
+end
+# tag_list blank
+shared_examples_for 'fail create pic tag_list blank' do
+  scenario 'user increment 0' do
+    expect do
+      fill_in_pic_new_form(:pic, taglistblank: true)
+      click_button '登録する'
+    end.to change(Pic, :count).by(0)
     expect(page).to have_title('認定する | BESTMEN -1番だけをシェアしよう-')
     expect(page).to have_css('h1', text: '認定する')
   end
