a45c3caa20705fd98407eb4bf4efb32ad57e8550
Ryo Takazawa <ryotakazawaengineer@gmail.com>
2019-09-19 09:10:39 +0000

    added rspec for user signup

---
+++ spec/features/users_login_spec.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.feature 'UsersLogin', type: :feature do
+  include SupportModule
+
+  scenario 'login' do
+    before { visit new_user_registration_path }
+    it_behaves_like 'signup-form have right css'
+
+    context 'valid info' do
+      it_behaves_like 'success create user'
+    end
+
+    context 'invalid info' do
+      # 失敗 (increment: 0)
+      it_behaves_like 'fail create user'
+    end
+  end
+end
--- spec/support/shared_examples_for_user.rb
+++ spec/support/shared_examples_for_user.rb
@@ -25,24 +25,96 @@ shared_examples_for 'success create user' do
       fill_in_signup_form(:user) # => SupportModule を使用
       click_button '登録'
     end.to change(User, :count).by(1)
-    # メッセージ
-    # expect(page).to have_css('div.alert.alert-success', text: 'Welcome to the BESTONE!')
-    # ユーザープロフィールへredirect
     expect(page).to have_current_path(user_path(User.last))
     expect(current_path).to eq user_path(User.last)
   end
 end
+
 # failed
-shared_examples_for 'fail create user' do
+# all blank
+shared_examples_for 'fail create user all blank' do
   scenario 'user increment 0' do
     expect do
       visit new_user_registration_path
-      fill_in_signup_form(:user, invalid: true) # => SupportModule を使用
+      fill_in_signup_form(:user, allblank: true)
       click_button '登録'
     end.to change(User, :count).by(0)
-    # メッセージ
-    # expect(page).to have_css('div.alert.alert-danger', text: 'errors')
-    # サインアップページがrender
+    expect(page).to have_title('新規登録 | BESTMEN -1番だけをシェアしよう-')
+    expect(page).to have_css('h1', text: '新規登録')
+  end
+end
+#nameblank
+shared_examples_for 'fail create user name blank' do
+  scenario 'user increment 0' do
+    expect do
+      visit new_user_registration_path
+      fill_in_signup_form(:user, nameblank: true)
+      click_button '登録'
+    end.to change(User, :count).by(0)
+    expect(page).to have_title('新規登録 | BESTMEN -1番だけをシェアしよう-')
+    expect(page).to have_css('h1', text: '新規登録')
+  end
+end
+# email blank
+shared_examples_for 'fail create user email blank' do
+  scenario 'user increment 0' do
+    expect do
+      visit new_user_registration_path
+      fill_in_signup_form(:user, emailblank: true)
+      click_button '登録'
+    end.to change(User, :count).by(0)
+    expect(page).to have_title('新規登録 | BESTMEN -1番だけをシェアしよう-')
+    expect(page).to have_css('h1', text: '新規登録')
+  end
+end
+# password blank
+shared_examples_for 'fail create user password blank' do
+  scenario 'user increment 0' do
+    expect do
+      visit new_user_registration_path
+      fill_in_signup_form(:user, passwordblank: true)
+      click_button '登録'
+    end.to change(User, :count).by(0)
+    expect(page).to have_title('新規登録 | BESTMEN -1番だけをシェアしよう-')
+    expect(page).to have_css('h1', text: '新規登録')
+  end
+end
+# password confirmation blank
+shared_examples_for 'fail create user password confirmation blank' do
+  scenario 'user increment 0' do
+    expect do
+      visit new_user_registration_path
+      fill_in_signup_form(:user, passwordconfirmationblank: true)
+      click_button '登録'
+    end.to change(User, :count).by(0)
+    expect(page).to have_title('新規登録 | BESTMEN -1番だけをシェアしよう-')
+    expect(page).to have_css('h1', text: '新規登録')
+  end
+end
+# not unique email
+shared_examples_for 'fail create user not unique email' do
+  scenario 'user increment 1 not 2' do
+    expect do
+      visit new_user_registration_path
+      fill_in_signup_form(:user)
+      click_button '登録'
+      visit sign_out_path
+      visit new_user_registration_path
+      fill_in_signup_form(:user)
+      click_button '登録'
+    end.to change(User, :count).by(1)
+    expect(page).to have_title('新規登録 | BESTMEN -1番だけをシェアしよう-')
+    expect(page).to have_css('h1', text: '新規登録')
+  end
+end
+# unmatch password confirmation
+shared_examples_for 'fail create user unmatch password' do
+  scenario 'user increment 0' do
+    expect do
+      visit new_user_registration_path
+      fill_in_signup_form(:user, unmatchpassword: true)
+      click_button '登録'
+    end.to change(User, :count).by(0)
     expect(page).to have_title('新規登録 | BESTMEN -1番だけをシェアしよう-')
     expect(page).to have_css('h1', text: '新規登録')
   end
