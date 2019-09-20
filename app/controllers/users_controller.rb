19bbc95d531e77a9844511422ed3bc9efb3b4f21
Ryo Takazawa <ryotakazawaengineer@gmail.com>
2019-09-20 04:35:19 +0000

    edited view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,8 +7,9 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find_by(id: params[:id])
-    @posted_pics = Pic.where(user_id: params[:id]).includes(:taggings).page(params[:page]).per(6)
+    @pics_per_page = 9
+    @posted_pics = Pic.where(user_id: params[:id]).includes(:taggings).page(params[:page]).per(@pics_per_page)
     @liked_pics = @user.votes.up.for_type(Pic).votables
-    @liked_pics = Kaminari.paginate_array(@liked_pics).page(params[:page]).per(6)
+    @liked_pics = Kaminari.paginate_array(@liked_pics).page(params[:page]).per(@pics_per_page)
   end
 end
