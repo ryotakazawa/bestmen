98776cabfbabbdcd700bbebac5d432aec9387b30
Ryo Takazawa <ryotakazawaengineer@gmail.com>
2019-09-21 14:41:20 +0000

    added rspec for comment #15

--- spec/factories/pics.rb
+++ spec/factories/pics.rb
@@ -2,8 +2,10 @@
 
 FactoryBot.define do
   factory :pic do
+    user_id { 1 }
     title { 'MyString' }
     address {'東京都港区芝公園４丁目２−８'}
     description { 'MyText' }
+    tag_list { '激辛系' }
   end
 end
