3843d9fbc6b7bee4ecc672f1c4ca225192257d7e
Ryo Takazawa <ryotakazawaengineer@gmail.com>
2019-09-23 13:15:59 +0000

    now only correct user can delete pic #24

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -33,7 +33,7 @@ class PicsController < ApplicationController
     @pic = current_user.pics.build(pic_params)
 
     if @pic.save
-      redirect_to @pic, notice: '投稿されました!'
+      redirect_to @pic, notice: '投稿されました'
     else
       render 'new'
     end
