b5b40f547afba6d9448c8503517b11035a0b46b0
Ryo Takazawa <ryotakazawaengineer@gmail.com>
2020-03-19 23:20:10 +0900

    Rubocopped

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -3,10 +3,10 @@
 require 'rails_helper'
 
 RSpec.describe CommentsController, type: :controller do
-  #describe 'GET #create' do
+  # describe 'GET #create' do
   #  it 'returns http success' do
   #    get :create
   #    expect(response).to have_http_status(:success)
   #  end
-  #end
+  # end
 end
